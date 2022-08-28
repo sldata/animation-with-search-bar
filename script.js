e57a86eab435f4896cd3e819d83dfe930feefd6c
Slawek01~ <lilcrunker@gmail.com>
2022-08-28 15:20:30 +0200

    created project

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('.search-btn')
+const input = document.querySelector('.search-input')
+
+
+const showSearchBar = () => {
+    input.classList.toggle('active')
+}
+
+btn.addEventListener('click', showSearchBar)
